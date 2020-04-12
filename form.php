f5b8c8df4d6c5edb0bce9e3a06c40884523adc2f
kumarishivangini <38472760+kumarishivangini@users.noreply.github.com>
2020-04-12 13:39:20 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+$username = "";
+$email    = "";
+$errors = array(); 
+
+$db = mysqli_connect('localhost', 'root', '', 'test');
+
+if (isset($_POST['submit'])) {
+
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $copassword = mysqli_real_escape_string($db, $_POST['password2']);
+
+
+ if ($password != $copassword) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  $user_check_query = "SELECT * FROM signup WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { 
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+
+  if (count($errors) == 0) {
+  	$password = ($password);
+
+  	$query = "INSERT INTO signup (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
+  	mysqli_query($db, $query);
+$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+        exit;
+	
+  }
+}
+?>
